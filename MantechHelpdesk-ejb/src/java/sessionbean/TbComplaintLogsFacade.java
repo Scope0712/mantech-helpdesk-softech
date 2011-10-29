a266bc1fdb75b2ba08a44d43210b3ead899b0678
thanhtuyen1710 <thanhtuyen1710@ea224837-4000-7568-c046-26c1ae152be1>
2011-10-29 14:32:21 +0000

    assign complaint

--- MantechHelpdesk-ejb/src/java/sessionbean/TbComplaintLogsFacade.java
+++ MantechHelpdesk-ejb/src/java/sessionbean/TbComplaintLogsFacade.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sessionbean;
 
 import entity.TbComplaintLogs;
@@ -16,6 +15,7 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class TbComplaintLogsFacade extends AbstractFacade<TbComplaintLogs> implements TbComplaintLogsFacadeLocal {
+
     @PersistenceContext(unitName = "MantechHelpdesk-ejbPU")
     private EntityManager em;
 
@@ -27,4 +27,9 @@ public class TbComplaintLogsFacade extends AbstractFacade<TbComplaintLogs> imple
         super(TbComplaintLogs.class);
     }
 
+    public int findResendNo(String complaintId) {
+         return (Integer) em.createNativeQuery("select max(resend_no)from tbComplaintLogs where complaint_Id = ?1").setParameter(1, complaintId).getSingleResult();       
+       // return (Integer) em.createQuery("SELECT MAX(x.resendNo) FROM TbComplaintLogs x WHERE x.TbComplaintLogsPK.complaintId = :complaintId").setParameter("complaintId", complaintId).getSingleResult();
+       
+    }
 }
