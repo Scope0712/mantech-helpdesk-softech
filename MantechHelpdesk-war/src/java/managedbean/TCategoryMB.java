4589e9e4dc52c07127a7b3a216ae2fee9919a225
thanhtuyen1710 <thanhtuyen1710@ea224837-4000-7568-c046-26c1ae152be1>
2011-10-30 17:30:16 +0000

    Add ManageCategories.xhtml Modify TCategoryMB.java

--- MantechHelpdesk-war/src/java/managedbean/TCategoryMB.java
+++ MantechHelpdesk-war/src/java/managedbean/TCategoryMB.java
@@ -2,28 +2,85 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package managedbean;
 
 import entity.TbCategories;
+import java.util.List;
+import java.util.Vector;
 import javax.ejb.EJB;
 import sessionbean.TbCategoriesFacadeLocal;
-
 
 /**
  *
  * @author tuyenbui
  */
 public class TCategoryMB {
+
     @EJB
     private TbCategoriesFacadeLocal tbCategoriesFacade;
+//    private List<TbCategories> tbcategoryList;
+    private static List<Category> categoryList;
 
     /** Creates a new instance of TCategoryMB */
-    public TCategoryMB() {
+    public TCategoryMB() {        
     }
 
-    public TbCategories findCategory(String categoryId){
+    public List<Category> getCategoryList() {
+        List<TbCategories> list = tbCategoriesFacade.findAll();
+        //categoryList = new Vector<Category>();
+        if (categoryList == null)categoryList = new Vector<Category>();
+        if (categoryList.isEmpty()) {
+            for (TbCategories c : list) {
+                categoryList.add(new Category(c));
+            }
+        }
+        return categoryList;
+    }
+
+    public TbCategories findCategory(String categoryId) {
         return tbCategoriesFacade.find(categoryId);
     }
 
+    public String saveAction() {
+        System.out.println("goi saveAction");
+        for (Category c : categoryList) {
+            c.setEditable(false);
+            System.out.println("category: id "+c.category.getCategoryId()+" Name:"+c.category.getCategoryName()+" priority:"+c.category.getPriority());
+            tbCategoriesFacade.edit(c.category);
+        }
+        //return current page
+        return null;
+    }
+
+    public String editAction(Category category) {
+        category.setEditable(true);
+        //return current page
+        return null;
+    }
+
+    public static class Category {
+
+        TbCategories category;
+        boolean editable;
+
+        public Category(TbCategories category) {
+            this.category = category;
+        }
+
+        public TbCategories getCategory() {
+            return category;
+        }
+
+        public void setCategory(TbCategories category) {
+            this.category = category;
+        }
+
+        public boolean isEditable() {
+            return editable;
+        }
+
+        public void setEditable(boolean editable) {
+            this.editable = editable;
+        }
+    }
 }
