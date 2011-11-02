20be558af21175de44604e02be5d8c0b18dc5c9a
vtthanh306 <vtthanh306@gmail.com>
2011-11-02 08:31:30 +0000

    ComplaintDetails sessionbean

---
+++ MantechHelpdesk-ejb/src/java/sessionbean/ComplaintDetailsFacade.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sessionbean;
+
+import entity.ComplaintDetails;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author THANH
+ */
+@Stateless
+public class ComplaintDetailsFacade extends AbstractFacade<ComplaintDetails> implements ComplaintDetailsFacadeLocal {
+    @PersistenceContext(unitName = "MantechHelpdesk-ejbPU")
+    private EntityManager em;
+
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ComplaintDetailsFacade() {
+        super(ComplaintDetails.class);
+    }
+    @Override
+    public List<ComplaintDetails> findByEmployeeID(String id) {
+        try{
+        return (List<ComplaintDetails>) em.createNamedQuery("ComplaintDetails.findByEmployeeID").setParameter("employeeID", id).getResultList();
+        }catch(Exception ex){
+            System.out.println("error");
+            return null;
+        }
+
+    }
+    public List<ComplaintDetails> findComplaintStatus(String id,String status) {
+        try{
+        return (List<ComplaintDetails>) em.createNamedQuery("ComplaintDetails.findComplaintStatus").setParameter("statusId", status).setParameter("employeeID", id).getResultList();
+        }catch(Exception ex){
+            System.out.println("error");
+            return null;
+        }
+
+    }
+
+}
