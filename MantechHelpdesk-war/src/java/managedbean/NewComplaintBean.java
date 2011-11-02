cc52097de2704bd0ea9c7601587368c18c0b87ea
vtthanh306 <vtthanh306@gmail.com>
2011-11-02 08:34:13 +0000

    manager bean

--- MantechHelpdesk-war/src/java/managedbean/NewComplaintBean.java
+++ MantechHelpdesk-war/src/java/managedbean/NewComplaintBean.java
@@ -6,14 +6,18 @@ package managedbean;
 
 import entity.TbCategories;
 import entity.TbComplaints;
+import entity.TbDepartments;
 import entity.TbStaffs;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import sessionbean.TbCategoriesFacadeLocal;
+import sessionbean.TbComplaintStatusFacadeLocal;
 import sessionbean.TbComplaintsFacadeLocal;
+import sessionbean.TbDepartmentsFacadeLocal;
 import sessionbean.TbStaffsFacadeLocal;
 
 /**
@@ -25,77 +29,86 @@ import sessionbean.TbStaffsFacadeLocal;
 public class NewComplaintBean {
 
     @EJB
+    private TbComplaintStatusFacadeLocal tbComplaintStatusFacade;
+    @EJB
+    private TbDepartmentsFacadeLocal tbDepartmentsFacade;
+    @EJB
     private TbStaffsFacadeLocal tbStaffsFacade;
     @EJB
     private TbCategoriesFacadeLocal tbCategoriesFacade;
     @EJB
     private TbComplaintsFacadeLocal tbComplaintsFacade;
-
     /** Creates a new instance of NewComplaintBean */
+    TbCategories category;
+    Collection<TbCategories> ListCategory;
+    TbComplaints complaint;
+    TbDepartments department;
+    Collection<TbDepartments> ListDepartment;
+
+    public Collection<TbDepartments> getListDepartment() {
+        return tbDepartmentsFacade.findAll();
+    }
+
+    public void setListDepartment(Collection<TbDepartments> ListDepartment) {
+        this.ListDepartment = ListDepartment;
+    }
+
+    public TbDepartments getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(TbDepartments department) {
+        this.department = department;
+    }
+
+    public TbComplaints getComplaint() {
+        return complaint;
+    }
+
+    public void setComplaint(TbComplaints complaint) {
+        this.complaint = complaint;
+    }
+
+    public Collection<TbCategories> getListCategory() {
+        return tbCategoriesFacade.findAll();
+    }
+
+    public void setListCategory(Collection<TbCategories> ListCategory) {
+        this.ListCategory = ListCategory;
+    }
+
+    public TbCategories getCategory() {
+        return category;
+    }
+
+    public void setCategory(TbCategories category) {
+        this.category = category;
+    }
+
     public NewComplaintBean() {
-    }
-    String Categories;
-    String Content;
-    String Image;
-    String EmployeeID;
-    int Priority;
+        category = new TbCategories();
+        complaint = new TbComplaints();
+        department = new TbDepartments();
 
-    public String getCategories() {
-        return Categories;
-    }
-
-    public void setCategories(String Categories) {
-        this.Categories = Categories;
-    }
-
-    public String getContent() {
-        return Content;
-    }
-
-    public void setContent(String Content) {
-        this.Content = Content;
-    }
-
-    public String getEmployeeID() {
-        return EmployeeID;
-    }
-
-    public void setEmployeeID(String EmployeeID) {
-        this.EmployeeID = EmployeeID;
-    }
-
-    public String getImage() {
-        return Image;
-    }
-
-    public void setImage(String Image) {
-        this.Image = Image;
-    }
-
-    public int getPriority() {
-        return Priority;
-    }
-
-    public void setPriority(int Priority) {
-        this.Priority = Priority;
     }
 
     public String NewComplaint() {
-        
-            try {
-                TbComplaints complaint = new TbComplaints();
-                complaint.setProblemContent(Content);
-                complaint.setImage(null);
-                TbCategories ca = tbCategoriesFacade.find(Categories);
-                complaint.setTbCategories(ca);
-                TbStaffs employee = tbStaffsFacade.find("Staff00001");
-                complaint.setTbStaffs(employee);
-                complaint.setPriorityValue(2);
-                tbComplaintsFacade.create(complaint);
-            } catch (Exception ex) {
-                Logger.getLogger(NewComplaintBean.class.getName()).log(Level.SEVERE, null, ex);
-            }
-       
-        return "/Home";
+
+        try {
+
+            //create new complaint
+            complaint.setTbCategories(category);
+            TbStaffs employee = tbStaffsFacade.find("Staff00001");
+            complaint.setTbStaffs(employee);
+            TbDepartments a = tbDepartmentsFacade.find(department.getDepartmentId());
+            TbCategories b = tbCategoriesFacade.find(category.getCategoryId());
+            int priority = a.getPriority() + b.getPriority();
+            complaint.setPriorityValue(priority);
+            tbComplaintsFacade.create(complaint);
+        } catch (Exception ex) {
+            Logger.getLogger(NewComplaintBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return "/Result?a=xong";
     }
 }
