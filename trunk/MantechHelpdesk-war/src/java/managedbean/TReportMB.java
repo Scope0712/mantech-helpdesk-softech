7c2e745eda62497e37437c3b8f03385eaa63a6dc
thanhtuyen1710@gmail.com <thanhtuyen1710@gmail.com@ea224837-4000-7568-c046-26c1ae152be1>
2011-11-04 15:11:24 +0000

    detail report

--- trunk/MantechHelpdesk-war/src/java/managedbean/TReportMB.java
+++ trunk/MantechHelpdesk-war/src/java/managedbean/TReportMB.java
@@ -10,16 +10,18 @@ import entity.TbCategories;
 import entity.TbDepartments;
 import entity.TbStaffs;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.SelectItem;
 import sessionbean.ReportFacadeLocal;
 import sessionbean.TbAccountsFacadeLocal;
 import sessionbean.TbCategoriesFacadeLocal;
 import sessionbean.TbDepartmentsFacadeLocal;
+import util.Week;
 
 /**
  *
@@ -44,10 +46,9 @@ public class TReportMB {
     private TbDepartments department;
     private String summaryreport;
     private List<Report> detailreport;
-    private String selectedValue;
-    //private List options;
-    //private SelectItem[] options;
-    private boolean rendered = false;
+    private String filterValue;   
+    private boolean filterDisabled = true;
+    private boolean filterValueDisabled = true;
 
     /** Creates a new instance of TReportMB */
     public TReportMB() {
@@ -70,19 +71,24 @@ public class TReportMB {
     }
 
     public List<Report> getDetailreport() {
-        if (filter != null) {
-            if (filter.equals("Category")) {
-                System.out.println("get detail report for category : "+selectedValue);
-                detailreport = reportFacade.findReportByCategory(selectedValue);
-            } else if (filter.equals("Technician")) {
-                System.out.println("get detail report for Technician: "+selectedValue);
-                detailreport = reportFacade.findReportByTechnician(selectedValue);
-            } else if (filter.equals("Department")){
-                System.out.println("get detail report for department: "+selectedValue);
-                detailreport = reportFacade.findReportByDepartment(selectedValue);
+        System.out.println("get Report table");
+        System.out.println(filter);
+        System.out.println(filterValue);
+        System.out.println(periodType);
+        if (filter != null && filterValue != null) {
+            if (periodType.equals("Daily")) {
+                detailreport = reportFacade.findDailyReportWithFilter(new Date(), filter, filterValue);
+                System.out.println("goi session bean daily");
+            } else if (periodType.equals("Weekly")) {
+                Date weekStartDate = Week.getWeekStartDate(new Date());
+                Date weekEndDate = Week.getWeekEndDate(new Date());
+                detailreport = reportFacade.findWeeklyReportWithFilter(weekStartDate, weekEndDate,filter, filterValue);
+            } else if (periodType.equals("Monthly")) {
+                Calendar cal = Calendar.getInstance();
+                detailreport = reportFacade.findMonthlyReportWithFilter(cal.get(Calendar.MONTH)+1, cal.get(Calendar.YEAR), filter, filterValue);
             }
-
         }
+
         return detailreport;
     }
 
@@ -96,7 +102,7 @@ public class TReportMB {
 
     public void setFilter(String filter) {
         this.filter = filter;
-        rendered = true;
+        filterValueDisabled = false;
     }
 
     public String getPeriodType() {
@@ -105,6 +111,7 @@ public class TReportMB {
 
     public void setPeriodType(String periodType) {
         this.periodType = periodType;
+        filterDisabled = false;
     }
 
     public String getSummaryreport() {
@@ -123,38 +130,58 @@ public class TReportMB {
         this.technician = technician;
     }
 
-    public String getSelectedValue() {
-        return selectedValue;
+    public String getFilterValue() {
+        return filterValue;
     }
 
-    public void setSelectedValue(String selectedValue) {
-        this.selectedValue = selectedValue;
+    public void setFilterValue(String filterValue) {
+        this.filterValue = filterValue;
+    }
+
+    public boolean isFilterDisabled() {
+        return filterDisabled;
+    }
+
+    public void setFilterDisabled(boolean filterDisabled) {
+        this.filterDisabled = filterDisabled;
+    }
+
+    public boolean isFilterValueDisabled() {
+        return filterValueDisabled;
+    }
+
+    public void setFilterValueDisabled(boolean filterValueDisabled) {
+        this.filterValueDisabled = filterValueDisabled;
     }
 
     public List<SelectItem> getOptions() {
         System.out.println("get Options");
         System.out.println(filter);
-        System.out.println(rendered);
+        System.out.println(filterValueDisabled);
         List<SelectItem> options = new ArrayList<SelectItem>();
-        options.add(new SelectItem("--Select--"));
-        if (filter != null && rendered) {
-            if (filter.equals("Category")) {
+
+        if (filter != null && !filterValueDisabled) {
+            if (filter.equals("Category_Id")) {
+                options.add(new SelectItem("--Select Category--"));
                 List<TbCategories> cgrList = tbCategoriesFacade.findAll();
                 for (TbCategories cgr : cgrList) {
                     options.add(new SelectItem(cgr.getCategoryId(), cgr.getCategoryName()));
                 }
-            } else if (filter.equals("Technician")) {
-                //get available technician
+            } else if (filter.equals("Technician_Id")) {
+                options.add(new SelectItem("--Select Technician--"));
                 List<TbAccounts> technicianList = tbAccountsFacade.findByRoleAndStatus("Roles00002", "Enable");
                 for (TbAccounts acc : technicianList) {
                     options.add(new SelectItem(acc.getTbStaffs().getStaffId(), acc.getTbStaffs().getStaffId()));
                 }
-            } else if (filter.equals("Department")) {                
+            } else if (filter.equals("Department_Id")) {
+                options.add(new SelectItem("--Select Department--"));
                 List<TbDepartments> deptList = tbDepartmentsFacade.findAll();
                 for (TbDepartments dept : deptList) {
                     options.add(new SelectItem(dept.getDepartmentId(), dept.getDepartmentName()));
                 }
             }
+        } else {
+            options.add(new SelectItem("--No options--"));
         }
         for (SelectItem item : options) {
             System.out.println("Label:" + item.getLabel() + " - Value:" + item.getValue().toString());
@@ -163,20 +190,12 @@ public class TReportMB {
         return options;
     }
 
-//    public void setOptions(SelectItem[] options) {
-//        this.options = options;
-//    }
-    public boolean isRendered() {
-        return rendered;
-    }
-
-    public void setRendered(boolean rendered) {
-        this.rendered = rendered;
-    }
 
     public List<Report> getReportList() {
         return reportFacade.findAll();
+    }
 
-
+    public List<Report> getDailyList() {
+        return reportFacade.findDailyReportWithFilter(new Date(), "Department_Id", "Depar00002");
     }
 }
